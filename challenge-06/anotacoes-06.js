db23d162941c08910e934177730548cfb7ca47fd
Daniel Lima <fr.daniel.lima@gmail.com>
2017-08-29 11:37:50 -0300

    Aula 06

---
+++ challenge-06/anotacoes-06.js
@@ -0,0 +1,39 @@
+/*
+    Operador vírgula;
+    var a, b = 2, c;
+
+    function myFunction(){
+        var x = 0;
+        return (x += 1, x);
+        // x recebe um e a função retorna x
+    }
+
+    Estrutura Condicional Switch;
+    function myFunction(x){
+        switch(x){
+            case 1:
+                console.log('x é 1');
+                break;
+            case 2:
+                console.log('x é 2');
+                break;
+            default:
+                console.log('x não é nem 1 nem 2);
+        }
+    }
+
+    Estrutura de Repetição (Loop) While
+    var counter = 0;
+
+    while(counter < 10){
+        console.log(counter);
+        counter++;
+    }
+
+    var counter = 10;
+
+    while( counter ){
+        console.log( counter-- );
+    }
+
+*/
