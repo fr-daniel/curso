1b85a93b8b0bafab12a3b2d4a8df9ca54ea51ae8
Daniel Lima <fr.daniel.lima@gmail.com>
2017-10-14 18:36:38 -0300

    Revisão Conteúdo - Desafio 25

---
+++ challenge-25/js/events/network_events.js
@@ -0,0 +1,14 @@
+(function( win, cod ){
+    'use strict';
+
+    /*
+        nagivator.onLine : true ou false
+    */
+    
+    if( navigator.onLine )
+        alert( 'Você está online!' );
+    else
+        alert( 'Você está offline!' );
+    
+
+})( window, document );
---
+++ challenge-25/js/events/resource_events.js
@@ -0,0 +1,8 @@
+(function( win, doc ){
+    'use strict';
+
+    win.addEventListener( 'load', function( event ){
+        alert( 'Janela carregada!' );
+    }, false );
+
+})( window, document );
