106a5f11444bee80bcc9b1b3402db9458295b7b7
Daniel Lima <fr.daniel.lima@gmail.com>
2017-10-16 15:15:02 -0300

    Desafio 26

---
+++ challenge-26/js/dom.js
@@ -0,0 +1,34 @@
+(function(){
+  'use strict';
+
+  function DOM( elements ){
+    this.element = document.querySelectorAll( elements );
+  }
+
+  DOM.prototype.on = function on( eventType, callback ){
+    Array.prototype.forEach.call( this.element, function( element ){
+      element.addEventListener( eventType, callback, false );
+    });
+  };
+
+  DOM.prototype.off = function off( eventType, callback ){
+    Array.prototype.forEach.call( this.element, function( element ){
+      element.removeEventListener( eventType, callback, false );
+    });
+  };
+
+  DOM.prototype.get = function get(){
+    return this.element;
+  };
+
+  var $a = new DOM('[data-js="link"]');
+  $a.on( 'click', function handleClick( event ){
+    event.preventDefault();
+    alert('Clicou');
+    $a.off( 'click', handleClick );
+  });
+
+  console.log( 'Elementos selecionados: ' + $a.get() );
+  console.log('$a é filho de body?', $a.get()[0].parentNode === document.body);
+
+})();
