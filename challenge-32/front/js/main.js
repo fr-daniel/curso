c8138454800a29d86fdeb2a5f56968f0b8844a75
Daniel Lima <fr.daniel.lima@gmail.com>
2017-11-02 17:10:41 -0300

    Aula 32

--- challenge-32/front/js/main.js
+++ challenge-32/front/js/main.js
@@ -1,12 +1,40 @@
 (function(){
   'use strict';
 
+  /*
   var ajax = new XMLHttpRequest();
-  ajax.open('GET', 'http://localhost:3000/user/fernando');
+  ajax.open('GET', 'http://localhost:3000/user/larissa');
   ajax.send();
-  ajax.addEventListener( 'readystatechange', function( e ){
+
+  ajax.onreadystatechange = function( e ){
+    if( ajax.readyState === 2 ){
+      console.log( 'headers ok '); //pegar parte da requisição
+      ajax.abort();
+    }
+
     if( ajax.readyState === 4 )
       console.log( ajax.responseText, ajax.status );
-  }, false );
+  };
+  */
+
+  /*var ajax = new XMLHttpRequest();
+  ajax.open( 'POST', 'http://localhost:3000/user' );
+  ajax.setRequestHeader( 'Content-Type', 'application/x-www-form-urlencoded' );
+  ajax.send( 'username=maria&name=Maria&lastname=Araujo&age=23' );
+
+  console.log( 'Cadastrando usuário...' );
+  ajax.onreadystatechange = function( e ){
+    if( ajax.readyState === 4  )
+      console.log( 'Usuário cadastrado!', ajax.responseText );
+  };*/
+
+  var get = new XMLHttpRequest();
+  get.open( 'GET', 'http://localhost:3000/user/roberto' );
+  get.send();
+
+  get.onreadystatechange = function(){
+    if( get.readyState === 4 )
+      console.log( get.responseText );
+  };
 
 })();
--- challenge-32/rest-api/index.js
+++ challenge-32/rest-api/index.js
@@ -2,42 +2,76 @@
 
 var express = require('express');
 var cors = require('cors');
+var bodyParser = require('body-parser');
 var app = express();
 
-var users = {
-  daniel: {
-    nome: 'Daniel',
-    sobrenome: 'Lima',
-    idade: 19
+var users = [
+  {
+    username: 'daniel',
+    name: 'Daniel',
+    lastname: 'Lima',
+    age: 19
   },
-  joao: {
-    nome: 'João',
-    sobrenome: 'Silva',
-    idade: 34
+  {
+    username: 'joao',
+    name: 'João',
+    lastname: 'Silva',
+    age: 34
   },
-  larissa: {
-    nome: 'Larissa',
-    sobrenome: 'Lima',
-    idade: 21
+  {
+    username: 'larissa',
+    name: 'Larissa',
+    lastname: 'Lima',
+    age: 21
   }
-}
+]
 
+app.use( bodyParser.urlencoded( { extended: false } ) );
 app.use( cors() );
 
 app.get('/', function( req, res ){
-  res.send('<h1>Home</h1>');
+  res.json( { response: 'Home' } );
 });
 
 app.get('/user', function( req, res ){
-  res.send('user');
+  res.json( { responde: 'User' } );
 });
 
 app.get('/user/:username', function( req, res ){
   var username = req.params.username;
-  if( users[username] )
-    return res.json(users[username]);
+  var hasUser = users.some( function( user ){
+    return user.username === username;
+  });
+
+  if( hasUser )
+    return res.json( users.filter( function( user ){
+      return user.username == username; 
+    })[0] );
   
   res.status(404).json( { error: 'Usuário não encontrado!' } );
+});
+
+app.post('/user', function( req, res ){
+  var username = req.body.username;
+  var name = req.body.name;
+  var lastname = req.body.lastname;
+  var age = req.body.age;
+
+  var hasUser = users.some( function( user ){
+    return user.username === username;
+  });
+
+  if( hasUser )
+    return res.json( users );
+
+  users.push({
+    username: username, 
+    name: name, lastname: 
+    lastname, 
+    age: age 
+  });
+
+  res.json( users );
 });
 
 app.listen(3000);
